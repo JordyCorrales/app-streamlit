af3f0d20d21cb9d2bc8ff20fdb746e3a4da5f9cf
Jordy Corrales <corrales.alexanderjc@gmail.com>
2025-05-22 20:55:19 -0500

    desde docker hub

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 
-URL = "https://model-flask-docker-wrji.onrender.com/predict/"
+#URL = "https://model-flask-docker-wrji.onrender.com/predict/"
+URL = "https://reroes-ml01-latest-ym5j.onrender.com/predict/"
